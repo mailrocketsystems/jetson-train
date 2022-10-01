c4a40460b3a92d31015bb3c36282d5d946cac141
Rocket Systems <mailrocketsystems@gmail.com>
2022-10-01 22:51:32 +0100

    Update result.py

--- result.py
+++ result.py
@@ -21,6 +21,26 @@ for file in fileLst:
     y.append(round(float(lossNum[0]), 3))
     x.append(file[3])
 
+bestChkpt = ""
+runOnce = True 
+prevLoss = 0.0
+for file in fileLst:
+    origFile = file
+    file = file.split("-")
+    loss = file[5]
+    lossNum = loss.split(".pth")
+    if runOnce:
+        prevLoss = lossNum
+        runOnce = False
+    else:
+        if lossNum < prevLoss:
+            prevLoss = lossNum
+            bestChkpt = origFile #file[0] + file[1] + file[2] + file[3] + file[4] + file[5]
+
+
+
+
+print("Bes Checkpoint: {}".format(bestChkpt))
 plt.plot(x, y)
 plt.xlabel("Epochs")
 plt.ylabel("Loss")
